76070d538df01411a33a384b8f2ca7e0fabd4e23
Joshua Ramnauth <joshua20050710@gmail.com>
2026-02-22 18:13:36 -0500

    modified layout

--- frontend/app/(visualizer)/layout.tsx
+++ frontend/app/(visualizer)/layout.tsx
@@ -7,24 +7,52 @@ export default function VisualizerLayout({
     return (
 
         <>
-        
+
         <header className="pl-8 pt-6 pb-6 border-b border-white space-y-2">
 
-            <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold">
 
-                Transformer Visualizer
+            Transformer Visualizer
 
-            </h1>
+        </h1>
 
-            <p className="text-sm text-gray-400 font-semibold">
+        <p className="text-sm text-gray-400 font-semibold">
 
-                VISUALIZING: Today , the weather is ___
+            VISUALIZING: Today , the weather is ___
 
-            </p>
+        </p>
 
         </header>
 
-        <div className></div>
+
+        <main className='relative min-h-screen'>
+
+        <div className="pl-8 pt-6 pb-6 space-y-2">
+
+        <p className="text-sm text-gray-400 font-semibold">
+        Phase 1: Embedding
+        </p>
+
+        <h2 className="text-xl font-bold" >
+
+        Step: is hardcoded right now
+
+        </h2>
+
+        </div>
+
+        {children}
+
+        <aside className = "absolute right-12 top-32 w-[330px] rounded-2xl border border-white p-6">
+
+        <p>
+        Filler Note
+        </p>
+
+        </aside>
+
+        </main>
+            
         
         </>
 
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -29,52 +29,7 @@ export default function RootLayout({
     <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}>
-
-        <header className="pl-8 pt-6 pb-6 border-b border-white space-y-2">
-
-           <h1 className="text-3xl font-bold">
-
-                Transformer Visualizer
-
-            </h1>
-
-            <p className="text-sm text-gray-400 font-semibold">
-
-                VISUALIZING: Today , the weather is ___
-
-            </p>
-
-        </header>
-
-        
-        <main className='relative'>
-
-          <div className="pl-8 pt-6 pb-6 space-y-2">
-
-            <p className="text-sm text-gray-400 font-semibold">
-              Phase 1: Embedding
-            </p>
-
-            <h2 className="text-xl font-bold" >
-
-              Step: is hardcoded right now
-
-            </h2>
-
-          </div>
-
-          {children}
-
-          <aside className = "absolute right-12 top-32 w-[330px] h-[330px] rounded-2xl border border-white p-6">
-
-            <p>
-              Filler Note
-            </p>
-
-          </aside>
-
-        </main>
-
+            {children}
        </body>
     </html>
   );
